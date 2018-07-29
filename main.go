1a675b24acf19e67f4de97f46d28c40817c658ac
Keith Rarick <kr@xph.us>
2018-07-28 18:31:56 -0700

    print usage and better error message formatting

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 )
 
 func main() {
+	if len(os.Args) != 3 {
+		fmt.Fprintln(os.Stderr, "usage: genbolt [input.go] [output.go]")
+		os.Exit(2)
+	}
+
 	in, out := os.Args[1], os.Args[2]
 	b, err := gen(in)
 	if err != nil {
-		panic(err)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 	err = ioutil.WriteFile(out, b, 0644)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }

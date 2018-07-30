ddf1336f7d96f822647ecb0a947ba0ed65cc680d
Keith Rarick <kr@xph.us>
2018-07-29 18:30:49 -0700

    take output file name as a flag

--- main.go
+++ main.go
@@ -1,26 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
 )
 
+var flagO = flag.String("o", "", "output `file` (default stdout)")
+
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Fprintln(os.Stderr, "usage: genbolt [input.go] [output.go]")
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage: genbolt [-o output.go] [input.go]")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	if flag.NArg() != 1 {
+		flag.Usage()
 		os.Exit(2)
 	}
 
-	in, out := os.Args[1], os.Args[2]
-	b, err := gen(in)
+	b, err := gen(flag.Arg(0))
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	err = ioutil.WriteFile(out, b, 0644)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+
+	if *flagO == "" {
+		os.Stdout.Write(b)
+	} else {
+		err = ioutil.WriteFile(os.Args[2], b, 0644)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 	}
 }
